c3083ec1c42cfd7a4ce40b03dca18aafe6e35144
Quinn Darling <quinndarling21@gmail.com>
2024-12-21 12:12:04 -0800

    Init

---
+++ XP/XPApp.swift
@@ -0,0 +1,17 @@
+//
+//  XPApp.swift
+//  XP
+//
+//  Created by Quinn Darling on 12/21/24.
+//
+
+import SwiftUI
+
+@main
+struct XPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
